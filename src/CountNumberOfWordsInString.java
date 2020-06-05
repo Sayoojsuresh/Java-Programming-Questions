d72d28c01c92786a0172ebd39367c9fae2fd4304
Sayooj Suresh <sayoojs3@gmail.com>
2020-06-06 00:45:13 +0530

    first commit

---
+++ src/CountNumberOfWordsInString.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class CountNumberOfWordsInString {
+
+	public static void main(String[] args) {
+		
+		String name="This is my First Java Programming Code";
+		
+		
+	String words[]=	name.split(" ");
+	
+	System.out.println(words.length);
+
+	}
+
+}
